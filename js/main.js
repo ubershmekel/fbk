d07fbf88afa9ba5af38fa3211599fb060de6b0b3
Yuval Greenfield <ubershmekel@gmail.com>
2013-10-23 22:23:39 -0700

    Fixed mixed tabs to spaces and prepared gameStates

--- js/main.js
+++ js/main.js
@@ -104,81 +104,110 @@ $(function() {
     
     init3D();
     
-	var game = {};
-	game.playerKeys = [{}, {}];
-	game.currentPlayer = 0;
-	game.taken = 't';
-	game.playerNames = {
-		0: 'Player Left',
-		1: 'Player Right'
-	};
-	game.playerColors = {
-		0: '#00f',
-		1: '#f00'
-	};
-	
-	game.pidToStr = function(pid) {
-		return "p" + pid;
-	}
-	
-	game.otherPlayer = function() {
-		return (game.currentPlayer + 1) % 2;
-	}
-	
-	game.removeKey = function(pid, key) {
-		delete game.playerKeys[pid][key];
-		var elemName = game.pidToStr(pid) + 'key' + key;
-		$('#' + elemName).remove();
-	}
+    var game = {};
+    game.playerKeys = [{}, {}];
+    game.taken = 't';
+    game.p0 = 0;
+    game.p1 = 1;
+    game.currentPlayer = game.p0;
+    game.playerNames = {};
+    game.playerNames[game.p0]= 'Player Left';
+    game.playerNames[game.p1]= 'Player Right';
+    game.playerColors = {};
+    game.playerColors[game.p0] = '#00f';
+    game.playerColors[game.p1] = '#f00';
+    
+    game.pidToStr = function(pid) {
+        return "p" + pid;
+    }
+    
+    game.otherPlayer = function() {
+        return (game.currentPlayer + 1) % 2;
+    }
+    
+    game.removeKey = function(pid, key) {
+        delete game.playerKeys[pid][key];
+        var elemName = game.pidToStr(pid) + 'key' + key;
+        $('#' + elemName).remove();
+    }
 
-	game.addKey = function(pid, key) {
-		game.playerKeys[pid][key] = game.taken;
-		
-		var elemName = game.pidToStr(pid) + 'key' + key;
-		var elem = $('<div/>', {
-			text: key,
-			id: elemName
-			});
-		
-		var holderId = '#' + game.pidToStr(pid) + 'keys';
-		console.log(holderId);
-		console.log(elemName);
-		$(holderId).append(elem);
-	}
+    game.keyHolderElem = function(pid) {
+        var holderId = '#' + game.pidToStr(pid) + 'keys';
+        return $(holderId);
+    }
+    
+    game.addKey = function(pid, key) {
+        game.playerKeys[pid][key] = game.taken;
+        
+        var elemName = game.pidToStr(pid) + 'key' + key;
+        var elem = $('<div/>', {
+            text: key,
+            id: elemName
+            });
+        
+        
+        game.keyHolderElem(pid).append(elem);
+    }
 
-	game.updateViz = function() {
-		$('#currentPlayer').text(game.playerNames[game.currentPlayer]).css('color', game.playerColors[game.currentPlayer]);
-		
-	}
-	
-	game.nextTurn = function() {
-		game.currentPlayer = game.otherPlayer();
-		game.updateViz();
-		
-		game.queueTurn();
-	}
-	
-	game.queueTurn = function() {
-	   setTimeout(game.nextTurn, 2000);
-	}
-	
-	
-	
-   $(window).keypress(function(e) {
-		var key = e.which;
-		console.log(key);
-		if(game.playerKeys[game.currentPlayer][key] == game.taken) {
-			// TODO: play wasted key sound
-			return;
-		}
-		if(game.playerKeys[game.otherPlayer()][key] == game.taken) {
-			// TODO: play conquer key sound
-			game.removeKey(game.otherPlayer(), key);
-		}
-		
-		game.addKey(game.currentPlayer, key);
-   });
+    game.updateViz = function() {
+        $('#currentPlayer').text(game.playerNames[game.currentPlayer]).css('color', game.playerColors[game.currentPlayer]);
+        
+    }
+    
+    game.nextTurn = function() {
+        game.currentPlayer = game.otherPlayer();
+        game.updateViz();
+        
+        game.queueTurn();
+    }
+    
+    game.queueTurn = function() {
+       setTimeout(game.nextTurn, 2000);
+    }
+    
+    
+    game.registerKeys = function() {
+        $(window).keypress(function(e) {
+            var key = e.which;
+            console.log(key);
+            if(game.playerKeys[game.currentPlayer][key] == game.taken) {
+                // TODO: play wasted key sound
+                return;
+            }
+            if(game.playerKeys[game.otherPlayer()][key] == game.taken) {
+                // TODO: play conquer key sound
+                game.removeKey(game.otherPlayer(), key);
+            }
+            
+            game.addKey(game.currentPlayer, key);
+        });
+    }
+    
+    game.stateChange = function(stateName) {
+        if (game.state != undefined) {
+            game.state.css('display', 'none');
+        }
+        game.state = $('#' + stateName);
+        game.state.css('display', 'block');
+    }
+    
+    game.newGame = function() {
+        game.stateChange('play');
+        game.playerKeys = [{}, {}];
+        game.currentPlayer = game.p0;
+        game.keyHolderElem(game.p0).empty();
+        game.keyHolderElem(game.p1).empty();
+        game.round = 1;
+        game.timeLeft = 5;
+    }
+    
+    game.main = function() {
+        game.stateChange('intro');
+        $('#startButton').click(function(){game.newGame();});
+        game.registerKeys();
+        game.updateViz();
+        game.queueTurn();
+    }
    
-   game.updateViz();
-   game.queueTurn();
+    game.main();
 });
